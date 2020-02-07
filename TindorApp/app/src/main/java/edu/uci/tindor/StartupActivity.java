96f2d43ba09b3cb6e6760d093ce091fbcc529ebd
Jing Chen <anniejingchen@gmail.com>
2020-02-08 00:02:22 +0800

    add flask-restful, add async ok-http

--- TindorApp/app/src/main/java/edu/uci/tindor/StartupActivity.java
+++ TindorApp/app/src/main/java/edu/uci/tindor/StartupActivity.java
@@ -3,15 +3,72 @@ package edu.uci.tindor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class StartupActivity extends AppCompatActivity {
 
+    public static String TAG = "StartupActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startup);
+
+        OkHttpClient client = new OkHttpClient();
+
+        String url = "http://192.168.101.12:8000/net_test/good";
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+                StartupActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        TextView myTextView = (TextView) findViewById(R.id.debugNetTextView);
+                        myTextView.setText("Connection failed. Please start the server!");
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    StartupActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            TextView myTextView = (TextView) findViewById(R.id.debugNetTextView);
+                            myTextView.setText("Connection failed. Please start the server!");
+                        }
+                    });
+                }
+                // Read data on the worker thread
+                final String responseData = response.body().string();
+
+                // Run view-related code back on the main thread
+                StartupActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        TextView myTextView = (TextView) findViewById(R.id.debugNetTextView);
+                        myTextView.setText(responseData);
+                    }
+                });
+            }
+        });
     }
 
     public void onLoginClicked(View view){
--- TindorServer/main.py
+++ TindorServer/main.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-def home():
-    return "hello"
 
-app.run(port=8000)
+class NetTest(Resource):
+    def get(self, name):
+        return {"net_test": name}
+
+api.add_resource(NetTest, '/net_test/<string:name>')
+
+app.run(host='0.0.0.0', port=8000)
