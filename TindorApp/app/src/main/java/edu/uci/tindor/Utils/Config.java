63811666600c4de00df1a759db3b0a29ffcad54a
Jing Chen <anniejingchen@gmail.com>
2020-02-18 00:36:20 +0800

    Revert "add port"
    This reverts commit caef586240cb230bb102c415247b711aa6b13712.

--- TindorApp/app/src/main/java/edu/uci/tindor/Utils/Config.java
+++ TindorApp/app/src/main/java/edu/uci/tindor/Utils/Config.java
@@ -1,6 +1,5 @@
 package edu.uci.tindor.Utils;
 
 public class Config {
-    //public static String BASE_URL = "http://35.236.111.125/";
-    public static String BASE_URL = "http://192.168.101.12:8000/";
+    public static String BASE_URL = "http://35.236.111.125/";
 }
